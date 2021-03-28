ca680f516f81166e50805947ec13c86dc6d6b7b7
FX <kmr06qwert18@icloud.com>
2021-03-29 06:29:34 +0900

    環境整えるのしんどかった

--- max_min.c
+++ max_min.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-void printMaxAndMin(int* input);
-
-int main(void){
-    int input[] = {1,2,3};
-    int size = sizeof(input);
-    printMaxAndMin(input, size);
-}
-
-void printMaxAndMin(int* input, int size){
-    int max = input[0], min = input[0];
-    for(int i = 1;i < size; ++i){
+void printMaxAndMin(int input[], int size){
+    int max = input[0], min = input[0], i = 1;
+    for(;i < size; i++){ //maxの探索
         if(max < input[i]){
             max = input[i];
         }
     }
-    for(int i = 1;i < size;++i){
+    for(;i < size; i++){ //minの探索
         if(min > input[i]){
             min = input[i];
         }
     }
+    printf("max = %d, min = %d", max, min);
+}
+
+int main(void){
+    int input[] = {7,4,1,2,3,-2};
+    int size = sizeof(input) / sizeof(input[0]);
+    printMaxAndMin(input, size);
 }
